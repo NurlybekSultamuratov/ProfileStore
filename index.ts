f7430bc4208fffc15f9ee2a11ba47d6981f4a39b
Nurlybek Sultamuratov <nsultamu@my.centennialcollege.ca>
2024-10-27 15:33:09 +0600

    updated yarn npm and packagejson and typescript node errors

--- index.ts
+++ index.ts
@@ -1,15 +1,11 @@
-import { createServer, ServerResponse } from 'node:http';
-import fs from 'fs';
-import path from 'path';
-import { dirname } from 'node:path';
-import { error } from 'node:console';
+import { createServer } from 'node:http';
 const hostname:string = '127.0.0.1';
 const port:number = 3000;
 
 
 
 
-const server= createServer((req, res) => {
+const server= createServer((req,res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('hello');
